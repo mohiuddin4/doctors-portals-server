1e2560e8d2c12a9a5584b96e12d759ac6a1af432
mohiuddin4 <golammohiuddin402@gmail.com>
2022-05-21 23:19:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,237 @@
+const express = require('express')
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+console.log(process.env.STRIPE_SECRET_KEY);
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zf7jw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized access" });
+    }
+
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        // console.log(err)
+        if (err) {
+            return res.status(403).send({ message: 'Forbiden accsess' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+async function run() {
+    try {
+        await client.connect();
+
+        const serviceColection = client.db('doctors-portals').collection('services');
+        const bookingColection = client.db('doctors-portals').collection('bookings');
+        const userColection = client.db('doctors-portals').collection('users');
+        const doctorColection = client.db('doctors-portals').collection('doctors');
+        const paymentColection = client.db('doctors-portals').collection('payments');
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+
+            const requesterAccount = await userColection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+        }
+
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const service = req.body;
+            const price = service.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceColection.find(query).project({ name: 1 });
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userColection.find().toArray();
+            res.send(users);
+        });
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userColection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        })
+
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+
+            const email = req.params.email;
+
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userColection.updateOne(filter, updateDoc);
+
+            res.send({ result });
+
+        });
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userColection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+
+            res.send({ result, token });
+        })
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+            //1
+            const services = await serviceColection.find().toArray();
+
+            //step2
+            const query = { date: date };
+            const bookings = await bookingColection.find(query).toArray();
+            // 3
+            // services.forEach(service => {
+            //     const serviceBookings = bookings.filter(b => b.treatment === service.name);
+            //     const booked = serviceBookings.map(s => s.slot);
+            //     const available = service.slots.filter(s => !booked.includes(s));
+
+            //     service.available = available;
+            //     // service.booked = serviceBookings.map(s => s.slot);
+            // })
+
+            //3
+            services.forEach(service => {
+                //4
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                //5
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                //6
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+                service.slots = available;
+            })
+
+            res.send(services);
+
+
+        })
+
+        app.get('/booking', verifyJWT, async (req, res) => {
+            const patient = req.query.patient;
+            // const authorization = req.headers.authorization;
+
+            // console.log('auth header', authorization);
+            const decodedEmail = req.decoded.email;
+            if (patient === decodedEmail) {
+                const query = { patient: patient };
+                const bookings = await bookingColection.find(query).toArray();
+                return res.send(bookings);
+            }
+            else {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+        })
+
+        app.get('/booking/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await bookingColection.findOne(query);
+            res.send(booking);
+        })
+
+        app.patch('/booking/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    paid: true,
+                    transectionId: payment.transectionId
+                }
+            }
+
+            const resule = await paymentColection.insertOne(payment);
+            const updatedBooking = await bookingColection.updateOne(filter, updateDoc);
+            res.send(updateDoc);
+        })
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+            const exists = await bookingColection.findOne(query);
+
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingColection.insertOne(booking);
+            return res.send({ success: true, result });
+        })
+
+        app.get('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+            const doctors = await doctorColection.find().toArray();
+            res.send(doctors)
+        });
+
+
+        app.post('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorColection.insertOne(doctor);
+            res.send(result);
+
+        })
+        app.delete('/doctor/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+
+            const result = await doctorColection.deleteOne(filter);
+            res.send(result);
+
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello from doctor uncle')
+})
+
+app.listen(port, () => {
+    console.log(`Doctrs app listening on port ${port}`)
+})
